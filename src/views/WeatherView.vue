45915aa93a505ac46d6871b6029703411908fef0
oskarcokl <oskar.cokl@gmail.com>
2023-08-20 11:24:20 +0200

    feat: Add debounce function.

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -1,6 +1,10 @@
 <template >
     <div class="container">
-        <div class="button" @click="clickHandler">Show weather for Ljubljana</div>
+        <!-- <div class="button" @click="clickHandler">Show weather for Ljubljana</div> -->
+        <div>
+            <div class="search-label">Search for city</div>
+            <input type="text" name="input" @keyup="debounceSearch">
+        </div>
         <div>
             <div>
                 Temperature: {{ temp }}
@@ -31,18 +35,31 @@ export default defineComponent({
             feelsLike: -1,
             tempMin: -1,
             tempMax: -1,
-            humidity: -1
+            humidity: -1,
+            debounceSearch: function(){},
         }
+    },
+    created() {
+        this.debounceSearch = this.debounce(() => this.search(), 1000);
     },
     methods: {
         async clickHandler() {
             const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Ljubljana&units=metric&appid=${import.meta.env.VITE_WEATHER_API}`);
-            console.log(res);
             this.temp = res.data.main.temp;
             this.feelsLike = res.data.main.feels_like;
             this.tempMin = res.data.main.temp_min;
             this.tempMax = res.data.main.temp_max;
             this.humidity = res.data.main.humidity;
+        },
+        debounce(func: Function, timeout = 300) {
+            let timer: number;
+            return () => {
+                clearTimeout(timer);
+                timer = setTimeout(() => { func(); }, timeout);
+            }
+        },
+        search() {
+            console.log('Searching for data');
         },
         fahrenheitToCelsius(fahrenheit: number) {
             return (fahrenheit - 32) / 1.8
@@ -51,6 +68,10 @@ export default defineComponent({
 });
 </script>
 <style>
+    .search-label {
+        text-align: center;
+    }
+
     .container {
         margin-top: 20px;
         display: flex;
